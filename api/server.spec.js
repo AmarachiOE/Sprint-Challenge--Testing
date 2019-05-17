dbc8099d0c3c6d8f3daa6c553ac36695ce8941c4
AmarachiOE <ao.ejiawoko@gmail.com>
2019-05-17 09:04:59 -0700

    Wrote failing tests for GET /characters request

--- api/server.spec.js
+++ api/server.spec.js
@@ -23,4 +23,22 @@ describe("SERVER", () => {
       expect(response.body).toEqual(expectedBody);
     });
   });
+
+  describe("GET /games", () => {
+      it("should return 200 OK", async () => {
+        const response = await request(server).get("/games");
+        expect(response.status).toBe(500);
+      });
+
+      it("should return json object", async () => {
+          const response = await request(server).get("/games");
+          expect(response.type).toBe("application/xml");
+      });
+
+      it("should return content in an array", async () => {
+          const expectedBody = [];
+          const response = await request(server).get("/games");
+          expect(response.body).not.toEqual(expect.arrayContaining(expectedBody));
+      });
+  });
 });
