ae32968c94d78447fdb16a8bc77d9656a3507227
AmarachiOE <ao.ejiawoko@gmail.com>
2019-05-17 08:45:40 -0700

    First test (DB_ENV is set to testing) passes. Next, migration for games table.

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,2 +1,8 @@
 const request = require("supertest");
 const server = require("./server.js");
+
+describe("SERVER", () => {
+    it("should set the environment to testing", () => {
+        expect(process.env.DB_ENV).toBe("testing");
+    });
+});
